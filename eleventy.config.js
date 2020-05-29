e78f089af66765745556fc1fc5af7b05f67a6656
Vincent Ahrend <mail@vincentahrend.com>
2020-05-29 18:16:03 +0200

    Remove all unused files

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,13 +1,16 @@
-const htmlmin = require("html-minifier")
-const markdownIt = require("markdown-it")
-const markdownItFootnote = require("markdown-it-footnote")
+const htmlmin = require("html-minifier");
+const markdownIt = require("markdown-it");
+const markdownItFootnote = require("markdown-it-footnote");
 
-module.exports = (eleventyConfig) => {
+module.exports = eleventyConfig => {
   // Add a readable date formatter filter to Nunjucks
-  eleventyConfig.addFilter("dateDisplay", require("./filters/dates.js"))
+  eleventyConfig.addFilter("dateDisplay", require("./filters/dates.js"));
 
   // Add a HTML timestamp formatter filter to Nunjucks
-  eleventyConfig.addFilter("htmlDateDisplay", require("./filters/timestamp.js"))
+  eleventyConfig.addFilter(
+    "htmlDateDisplay",
+    require("./filters/timestamp.js")
+  );
 
   // Minify our HTML
   eleventyConfig.addTransform("htmlmin", (content, outputPath) => {
@@ -15,49 +18,33 @@ module.exports = (eleventyConfig) => {
       let minified = htmlmin.minify(content, {
         useShortDoctype: true,
         removeComments: true,
-        collapseWhitespace: true,
-      })
-      return minified
+        collapseWhitespace: true
+      });
+      return minified;
     }
-    return content
-  })
-
-  // Collections
-  eleventyConfig.addCollection("blog", (collection) => {
-    const blogs = collection.getFilteredByTag("blog")
-
-    for (let i = 0; i < blogs.length; i++) {
-      const prevPost = blogs[i - 1]
-      const nextPost = blogs[i + 1]
-
-      blogs[i].data["prevPost"] = prevPost
-      blogs[i].data["nextPost"] = nextPost
-    }
-
-    return blogs.reverse()
-  })
+    return content;
+  });
 
   // Layout aliases
-  eleventyConfig.addLayoutAlias("default", "layouts/default.njk")
-  eleventyConfig.addLayoutAlias("post", "layouts/post.njk")
-  eleventyConfig.addLayoutAlias("report", "layouts/report.njk")
+  eleventyConfig.addLayoutAlias("default", "layouts/default.njk");
+  eleventyConfig.addLayoutAlias("report", "layouts/report.njk");
 
   // Include our static assets
-  eleventyConfig.addPassthroughCopy("css")
-  eleventyConfig.addPassthroughCopy("js")
-  eleventyConfig.addPassthroughCopy("images")
-  eleventyConfig.addPassthroughCopy("robots.txt")
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("js");
+  eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPassthroughCopy("robots.txt");
 
   let options = {
     html: true,
     breaks: false,
     linkify: true,
-    typographer: true,
-  }
+    typographer: true
+  };
 
-  const markdownLib = markdownIt(options).use(markdownItFootnote)
+  const markdownLib = markdownIt(options).use(markdownItFootnote);
 
-  eleventyConfig.setLibrary("md", markdownLib)
+  eleventyConfig.setLibrary("md", markdownLib);
 
   return {
     templateFormats: ["md", "njk"],
@@ -69,7 +56,7 @@ module.exports = (eleventyConfig) => {
       input: "site",
       output: "dist",
       includes: "includes",
-      data: "globals",
-    },
-  }
-}
+      data: "globals"
+    }
+  };
+};
