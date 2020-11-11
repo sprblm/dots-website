6afb5dc31514e61267a3199d9136af3c8044bbb5
Vincent Ahrend <mail@vincentahrend.com>
2020-11-11 22:47:34 +0100

    Add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "airbnb-base",
+    "prettier", // keep last
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  rules: {},
+};
