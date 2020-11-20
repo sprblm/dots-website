5265213992544a2b3ca641b3df7a79744eb14e8c
Vincent Ahrend <mail@vincentahrend.com>
2020-11-20 17:57:11 +0100

    Fix mobile nav

--- resources/js/modules/mobile-nav/index.js
+++ resources/js/modules/mobile-nav/index.js
@@ -1,17 +1,19 @@
 import $$ from "@utilities/selectors";
 
-const MobileNav = (function MobileNav() {
-  $$.mobileNavToggle.addEventListener("click", () => {
-    this.classList.toggle("menu-toggle-active");
-    $$.mobileNav.classList.toggle("menu-visible");
+function toggleMobileMenu() {
+  this.classList.toggle("menu-toggle-active");
+  $$.mobileNav.classList.toggle("menu-visible");
 
-    // set aria-expanded attribute on menu toggle button
-    if (this.getAttribute("aria-expanded") === "false") {
-      this.setAttribute("aria-expanded", "true");
-    } else {
-      this.setAttribute("aria-expanded", "false");
-    }
-  });
+  // set aria-expanded attribute on menu toggle button
+  if (this.getAttribute("aria-expanded") === "false") {
+    this.setAttribute("aria-expanded", "true");
+  } else {
+    this.setAttribute("aria-expanded", "false");
+  }
+}
+
+const MobileNav = (function MobileNav() {
+  $$.mobileNavToggle.addEventListener("click", toggleMobileMenu);
 })();
 
 export default MobileNav;
