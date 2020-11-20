2068a55d58ba07d190230426665314a47f8fc258
Vincent Ahrend <mail@vincentahrend.com>
2020-11-20 16:55:26 +0100

    Add custom webpack config

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const isDev = process.env.NODE_ENV !== "production";
+
+module.exports = {
+  devServer: {
+    clientLogLevel: "silent",
+    contentBase: path.join(__dirname, "dist"),
+    host: "0.0.0.0",
+    hot: true,
+    port: 8081,
+    progress: true,
+    stats: "minimal",
+    sockPort: 8081,
+    writeToDisk: true,
+  },
+  devtool: isDev ? "cheap-module-source-map" : "source-map",
+  entry: [
+    path.resolve(__dirname, "./resources/js/main.js"),
+    path.resolve(__dirname, "./resources/sass/main.scss"),
+  ],
+  mode: isDev ? "development" : "production",
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+          { loader: "css-loader", options: { url: false } },
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist", "js"),
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "../css/[name].css",
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@utilities": path.resolve(__dirname, "resources/js/utilities"),
+      "@modules": path.resolve(__dirname, "resources/js/modules"),
+    },
+  },
+};
