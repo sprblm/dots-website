b58f439321be653bb9cc516a839ef7e9ee4698ea
Vincent Ahrend <mail@vincentahrend.com>
2020-11-20 17:57:58 +0100

    Clean up package.json

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
-const tailwindcss = require("tailwindcss");
+/* eslint-disable import/no-extraneous-dependencies */
 const autoprefixer = require("autoprefixer");
+const tailwindcss = require("tailwindcss");
 
 module.exports = {
   plugins: [tailwindcss("./tailwind.config.js"), autoprefixer],
