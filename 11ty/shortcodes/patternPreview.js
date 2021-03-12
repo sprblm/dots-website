9495529b5687f3760d7227c44c2292f768e9b9fa
Vincent Ahrend <mail@vincentahrend.com>
2021-03-12 09:55:30 +0100

    Add illustrations with fallback to pattern thumbnails

--- 11ty/shortcodes/patternPreview.js
+++ 11ty/shortcodes/patternPreview.js
@@ -6,7 +6,13 @@
 module.exports = (pattern) => `
 <div class="pattern-preview">
   <a href="${pattern.data.page.url}">
-    <img width="322" height="204" src="/images/illustrations/placeholder.svg" />
+    <img
+      class="pattern-preview-illustration
+      // pseudo-image (transparent 1x1px)
+      src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
+      style="background-image: url(${
+        pattern.data.page.url
+      }/illustration.svg)" />
     <h3 class="my-4">${pattern.data.title}</h3>
     <p class="leading-snug text" >${pattern.data.description || ""}</p>
   </a>
