8853f32dc244946a1b842622c783b5bfa28a1e6b
Karissa McKelvey <633012+okdistribute@users.noreply.github.com>
2021-05-30 19:04:19 -0700

    chore: lint

--- 11ty/shortcodes/renderPatternText.js
+++ 11ty/shortcodes/renderPatternText.js
@@ -1,31 +1,35 @@
-const fs = require("fs")
+const fs = require("fs");
 const path = require("path");
-const YAML = require('yaml')
+const YAML = require("yaml");
 
 module.exports = (content) => {
-    let list = YAML.parse(fs.readFileSync(
-        path.join(__dirname, '..', '..', 'dots-patterns', 'glossary', 'list.yml').toString(), 'utf8'
-    ))
-    let glossaryRegex = '(?:\s'
-    let definitionByWord = {}
+  const list = YAML.parse(
+    fs.readFileSync(
+      path
+        .join(__dirname, "..", "..", "dots-patterns", "glossary", "list.yml")
+        .toString(),
+      "utf8"
+    )
+  );
+  let glossaryRegex = "(?:s";
+  const definitionByWord = {};
 
-    list.forEach(row => {
-        if (row.word.length > 0) {
-            glossaryRegex += row.word
-            definitionByWord[row.word.toLowerCase()] = row
-            definitionByWord[row.word.toLowerCase() + 's'] = row
-        }
-        glossaryRegex += '[s|\s]|'
-    })
+  list.forEach((row) => {
+    if (row.word.length > 0) {
+      glossaryRegex += row.word;
+      definitionByWord[row.word.toLowerCase()] = row;
+      definitionByWord[`${row.word.toLowerCase()}s`] = row;
+    }
+    glossaryRegex += "[s|s]|";
+  });
 
-    glossaryRegex = glossaryRegex.slice(0, glossaryRegex.length - 1)
-    glossaryRegex += ')'
+  glossaryRegex = glossaryRegex.slice(0, glossaryRegex.length - 1);
+  glossaryRegex += ")";
 
-    let re = new RegExp(glossaryRegex, "ig")
-    return content.replace(re, (match, index) => {
-        let def = definitionByWord[match]
-        console.log('got match', match, def)
-        if (!def) return match
-        return ` <span class="glossary">${match}</span><span class="glossary-definition">${def.definition}</span>`
-    })
-}
+  const re = new RegExp(glossaryRegex, "ig");
+  return content.replace(re, (match) => {
+    const def = definitionByWord[match];
+    if (!def) return match;
+    return ` <span class="glossary">${match}</span><span class="glossary-definition">${def.definition}</span>`;
+  });
+};
